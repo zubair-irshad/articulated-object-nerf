76adaf151053eec424e2fcc0d252aff325c6b392
Jianning Deng <s2262444@ed.ac.uk>
2023-08-27 23:46:51 +0000

    move data_gen script to another folder to avoid conflicts. introducing config file for data generation

--- datagen/data_gen.py
+++ datagen/data_gen.py
@@ -1,7 +1,33 @@
 import sapien
 from data_utils import *
 import argparse
+import json
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Data generation for NeRF training.")
+
+    parser.add_argument("--config", type=str, required=True, help="Path to configuration file.")
+    parser.add_argument("--urdf_file", type=str, help="file path to the urdf file of sapien")
+    parser.add_argument("--output_dir", type=str, help="path to save the generated images")
+    parser.add_argument("--resolution", type=int, default=[512, 512], nargs='+', help="Image resolution, w h, default: w = 512, h = 512")
+    
+    args = parser.parse_args()
+
+    # Load and parse the JSON configuration file
+    with open(args.config, "r") as config_file:
+        config_data = json.load(config_file)
+
+    required_args = ["urdf_file", "output_dir"]
+    missing_args = [arg for arg in required_args if arg not in config_data]
+    if missing_args:
+        raise ValueError(f"Required argument(s) {', '.join(missing_args)} not found in the JSON configuration")
+
+    # Update the args namespace with loaded JSON data
+    for key, value in config_data.items():
+        setattr(args, key, value)
+
+    return args
+        
 def main(args):
     engine = sapien.Engine()
     renderer = sapien.SapienRenderer(offscreen_only=True)
@@ -37,23 +63,18 @@ def main(args):
         far=far,
     )
 
-    output_path = args.output_path
-    P(output_path).mkdir(exist_ok=True)
+    output_path = P(args.output_dir)
+    output_path.mkdir(exist_ok=True, parents=True)
 
 
     splits = ('train', 'test', 'val')
-
+    print("generating images for training...")
     gen_articulated_object_nerf_s1(100, 4, 'train', camera, asset, scene, object_path=output_path)
+    print("generating images for validation...")
     gen_articulated_object_nerf_s1(50, 4, 'test', camera, asset, scene, object_path=output_path)
+    print("generating images for testing...")
     gen_articulated_object_nerf_s1(50, 4, 'val', camera, asset, scene, object_path=output_path)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Description of your program.")
-    
-    # Define command-line arguments
-    parser.add_argument("--urdf_file", type=str, required=True, help="Path to urdf file.")
-    parser.add_argument("--output_dir", type=str, required=True, help="Path to output directory.")
-    parser.add_argument("--resolution", type=list, default=[640, 480] , help="list of resolution, [w, h]")
-    
-    args = parser.parse_args()
+    args = parse_args()
     main(args)
